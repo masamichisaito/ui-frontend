e8474967ef24838583026da4eefbcf38c0acc677
masa.sai <masa.1985@icloud.com>
2025-05-15 07:59:02 +0900

    Add .env for Vite API connection in CI

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path'; //
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
   },
+  envDir: resolve(__dirname), // ← これを追加！
   server: {
     fs: {
       strict: false,
